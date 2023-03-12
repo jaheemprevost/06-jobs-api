e36f2a2d09b5aba4d5f59c015bf4c58494ab860d
jaheemprevost <jaheemdp@gmail.com>
2023-03-12 13:59:49 -0400

    Implement SwaggerUI API documentation

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 
+// Swagger 
+
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -35,8 +41,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
 
+app.get('/', (req, res) => {
+  res.send('<h1>Posts API</h1> <a href="/api-docs">Documentation</a>');
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 
 app.use('/api/v1/auth', authRouter);
